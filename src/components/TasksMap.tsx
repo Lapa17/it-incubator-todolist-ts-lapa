bbbdf79c66ba58de227d03d74690b13b671f89eb
Pavel Laparevich <laparevichpavel@gmail.com>
2021-11-16 23:55:15 +0300

    code refactoring

--- src/components/TasksMap.tsx
+++ src/components/TasksMap.tsx
@@ -7,37 +7,37 @@ import SuperSpan from "./SuperSpan";
 
 
 type TaskMapType = {
-    tasks : Array<TaskType>
+    tasks: Array<TaskType>
     todoListID: string
-    removeTask: (taskId: string, todoListID:string) => void
+    removeTask: (taskId: string, todoListID: string) => void
     changeTaskStatus: (check: CheckType, taskId: string, todoListID: string) => void
-    onNewTaskTitleChange: (title: string, id: string)=> void
+    onNewTaskTitleChange: (title: string, id: string) => void
 }
 
-const TasksMap = (props:TaskMapType) => {
+const TasksMap = (props: TaskMapType) => {
 
     return <ul>{
         props.tasks.map(t => {
-        const onClickHandler = () => props.removeTask(t.id, props.todoListID)
-        const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-            props.changeTaskStatus(e.currentTarget.checked, t.id, props.todoListID);
-        }
-        const onNewTaskTitleAdd = (newTitle:string) =>{
-            debugger
-            props.onNewTaskTitleChange(newTitle, t.id)
-        }  
+            const onClickHandler = () => props.removeTask(t.id, props.todoListID)
+            const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+                props.changeTaskStatus(e.currentTarget.checked, t.id, props.todoListID);
+            }
+            const onNewTaskTitleAdd = (newTitle: string) => {
+                debugger
+                props.onNewTaskTitleChange(newTitle, t.id)
+            }
 
 
-        return <li key={t.id} className={t.isDone ? "is-done" : ""}>
-            <input type="checkbox"
-                   onChange={onChangeHandler}
-                   checked={t.isDone}/>
-            <SuperSpan  title={t.title} onTitleChange={onNewTaskTitleAdd}/>
-            <Button onClick={onClickHandler} name={'x'}/>
-        </li>
-    })}
+            return <li key={t.id} className={t.isDone ? "is-done" : ""}>
+                <input type="checkbox"
+                    onChange={onChangeHandler}
+                    checked={t.isDone} />
+                <SuperSpan title={t.title} onTitleChange={onNewTaskTitleAdd} />
+                <Button onClick={onClickHandler} name={'x'} />
+            </li>
+        })}
 
-</ul>
+    </ul>
 }
 
 export default TasksMap;
